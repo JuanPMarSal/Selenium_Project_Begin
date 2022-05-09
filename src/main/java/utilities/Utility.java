2a91e083c12b0a9c059642123c4599f93cda0b30
JuanPMarSal <juanmarsal.pro@gmail.com>
2022-05-09 18:19:56 -0500

    Utility creation

---
+++ src/main/java/utilities/Utility.java
@@ -0,0 +1,14 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utility {
+    public static Object fetchPropertiesValues(String parameter) throws IOException {
+        FileInputStream file = new FileInputStream("config/ApplicationConfig.properties");
+        Properties property = new Properties();
+        property.load(file);
+        return property.get(parameter);
+    }
+}
